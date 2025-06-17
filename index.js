1fbf54d9b96d1a8a192305f09cafa94cbda933f8
Suman Srinivas <sumansrinivas@Sumans-MacBook-Pro.local>
2025-06-17 14:34:03 +0530

    crud routes were build

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 const Chat = require('./models/chat.js');
+const methodOverride = require('method-override');
 
 app.set('views',path.join(__dirname,'/views'));
 app.set('view engine','ejs');
 app.use(express.static(path.join(__dirname,'/public')));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 main().then(()=>{
     console.log('successfully connected');
@@ -54,6 +56,31 @@ app.post('/chats',(req,res)=>{
         console.log(err);
     });
 
+    res.redirect('/chats');
+});
+
+//Edit Route
+app.get('/chats/:id/edit',async(req,res)=>{
+    let {id} = req.params;
+    let chat = await Chat.findById(`${id}`);
+    res.render('edit.ejs',{chat});
+});
+
+//UPDATE Route
+app.put('/chats/:id',async(req,res)=>{
+    let {id} = req.params;
+    let {msg : newMsg} = req.body;
+    await Chat.findByIdAndUpdate({_id:`${id}`},
+        { msg : newMsg }, 
+        {runValidators : true, new : true});
+    res.redirect('/chats');
+});
+
+//DESTROY Route
+app.delete('/chats/:id',async(req,res)=>{
+    let {id} = req.params;
+    let deletedChart = await Chat.findByIdAndDelete({_id: `${id}`});
+    console.log(deletedChart);
     res.redirect('/chats');
 });
 
